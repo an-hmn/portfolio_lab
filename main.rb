1a1887f2832969a71221e4ed25728b33791bf163
=-kriss-= <kriss@modelcitizen.com.au>
2014-03-06 21:40:03 +1100

    updated main.rb with the LONG way around... short way wont work :(

--- main.rb
+++ main.rb
@@ -2,20 +2,57 @@ require "pry"
 require "sinatra"
 require "sinatra/reloader"
 
-# everything happens at /
-# possible TOP LEVEL params are home, about, contact
-get "/:page" do
-  # show homepage
-  erb params[:page]
+# error handlers
+not_found do 
+  erb :'404'
 end
-# possible STUDNET LEVEL params are anna, kriss, mark, tom (alphabetical)
-get "/student/:name" do
-  # show studnet page
-  erb params[:name]
+error { @error = request.env['sinatra_error'] ; haml :'500' }
+
+# possible TOP LEVEL params are home, about, contact
+get "/home" do 
+  erb :home
+end
+get "/about" do 
+  erb :about
+end
+get "/contact" do 
+  erb :contact
+end
+get "/student/anne" do 
+  erb :anne
+end
+get "/student/kriss" do 
+  erb :kriss
+end
+get "/student/mark" do 
+  erb :mark
+end
+get "/student/tom" do 
+  erb :tom
 end
 
+
+
+
+
+# Joel lied - this should work but it doesn't, which is why we have the extended version as above
+# get "/:page" do
+#   erb params[:page]
+# end
+# # possible STUDENT LEVEL params are anna, kriss, mark, tom (alphabetical)
+# get "/student/:name" do
+#   # show studnet page
+#   erb params[:name]
+# end
+
+
+
+
+
+# this should handle the contact form POST data
 post "/contact" do 
   # form mail handler here
 end
+
 
 
