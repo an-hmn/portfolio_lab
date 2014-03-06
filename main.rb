2071f83ecac00a8ad7b2edb9fa29db1b84a7d69d
=-kriss-= <kriss@modelcitizen.com.au>
2014-03-06 20:42:42 +1100

    updated layout with basic nav and cleaned up main.rb

--- main.rb
+++ main.rb
@@ -8,35 +8,14 @@ get "/:page" do
   # show homepage
   erb params[:page]
 end
-
-
-# get "/home" do
-#   # show homepage
-#   erb :home
-# end
-
-# get "/about" do
-#   # show homepage
-#   erb :home
-# end
-
-# get "/contact" do
-#   # show homepage
-#   erb :home
-# end
-
+# possible STUDNET LEVEL params are anna, kriss, mark, tom (alphabetical)
 get "/student/:name" do
   # show studnet page
   erb params[:name]
 end
 
-# just keeping the following lines for possible future reference
-#get parameters from the URL - two params, just step and answer
-#   step = params[:step].to_i
-#   answer = params[:answer]
-# #setup initial variables
-#   @step = 1
-#   @question = "<p>1. Do you have a test for that?</p>"
-#   @yesno = true
-#do the correct thing according to what 'step' you are up to
-#generally - update the variables to something new each time
+post "/contact" do 
+  # form mail handler here
+end
+
+
